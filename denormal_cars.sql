91f20b5b8f39c6d31cc24dd13b4961f793afe947
Mattdre <olsen_matthew@yahoo.com>
2016-07-20 10:59:02 -1000

    Finished number 7 of denormal

---
+++ denormal_cars.sql
@@ -0,0 +1,26 @@
+DROP USER IF EXISTS denormal_user;
+DROP DATABASE IF EXISTS denormal_cars;
+
+CREATE USER denormal_user;
+CREATE DATABASE denormal_cars OWNER denormal_user;
+
+\c denormal_cars;
+\i scripts/denormal_data.sql;
+-- --5--
+-- SELECT DISTINCT make_title
+-- FROM car_models;
+
+-- --6--
+-- SELECT DISTINCT model_title
+-- FROM car_models
+-- WHERE make_code = 'VOLKS';
+
+-- --7--
+-- SELECT
+--   make_code,
+--   model_code,
+--   model_title,
+--   year
+-- FROM car_models
+-- WHERE make_code = 'LAM';
+
